1b40d51afe9492cd045b8d3b5cb4c13a78b755bd
Zinont <zinont@bk.ru>
2022-09-27 20:54:33 +0400

    Add task solution

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string[] Array = {"eqw","heleo", "341","f","press"};
+string[] NewArray = new string[Array.Length];
+
+FindingElements(Array, NewArray);
+PrintArray(NewArray);
+
+
+
+void FindingElements(string[] array, string[] newarray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3 )
+        {
+            newarray[count] = array[i];
+            count++;
+        }
+    }
+
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
